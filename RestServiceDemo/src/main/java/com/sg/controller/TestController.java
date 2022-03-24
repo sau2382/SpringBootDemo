0dddd71cbc88d52eb0b5309399114fdf16c6515f
acces <acces@DESKTOP-UM2NUHR>
2022-03-24 23:31:34 +0530

    Open API implementation

--- RestServiceDemo/src/main/java/com/sg/controller/TestController.java
+++ RestServiceDemo/src/main/java/com/sg/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sg.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j // lombok
 public class TestController {
 	
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s from %s!";
+	
+	@Value("${spring.application.name}")
+	private String appName;
 	
 	@GetMapping("/test")
 	public Name test(@RequestParam(value = "name", defaultValue = "World") String name) {
 		log.info("Rest controller /test");
-		return new Name((long)1, String.format(template, name));
+		return new Name((long)1, String.format(template, name, appName));
 		
 	}
 
