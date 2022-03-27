e5f2cfba2590d1a00f0103321d6b55ebc8ba98c4
sau2382 <access.saurabh@gmail.com>
2022-03-27 12:43:56 +0530

    added refreshscope to the controller bean

--- RestServiceDemo/src/main/java/com/sg/controller/TestController.java
+++ RestServiceDemo/src/main/java/com/sg/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.sg.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j // lombok
+@RefreshScope  //refreshes config via actuator/refresh endpoint
 public class TestController {
 	
 	private static final String template = "Hello, %s from %s!";
