a03cf95b41d8f2988291cc421868a8370537d888
sau2382 <access.saurabh@gmail.com>
2022-04-11 21:59:27 +0530

    kubernetes changes

--- RestServiceDemo/src/main/java/com/sg/controller/TestController.java
+++ RestServiceDemo/src/main/java/com/sg/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.sg.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,10 @@ import lombok.extern.slf4j.Slf4j;
 @RefreshScope  //refreshes config via actuator/refresh endpoint
 public class TestController {
 	
-	private static final String template = "Hello, %s from %s!";
+	private static final String template = "Hello, %s from %s!  --  Host:%s  Port:%s";
+	
+	@Autowired
+	private Environment environment;
 	
 	@Value("${demoservice.welcometext}")
 	private String appName;
@@ -23,7 +28,9 @@ public class TestController {
 	@GetMapping("/test")
 	public Name test(@RequestParam(value = "name", defaultValue = "World") String name) {
 		log.info("Rest controller /test");
-		return new Name((long)1, String.format(template, name, appName));
+        String port = environment.getProperty("local.server.port");
+		String host = environment.getProperty("HOSTNAME");
+		return new Name((long)1, String.format(template, name, appName, host, port));
 		
 	}
 	
